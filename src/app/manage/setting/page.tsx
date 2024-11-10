67176cb42c4a67a3cd0da0a956cf1ded365ed346
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-11-10 22:24:35 +0700

    update Role in settings

--- src/app/manage/setting/page.tsx
+++ src/app/manage/setting/page.tsx
@@ -1,15 +1,23 @@
-import ChangePasswordForm from '@/app/manage/setting/change-password-form'
-import UpdateProfileForm from '@/app/manage/setting/update-profile-form'
-import { Badge } from '@/components/ui/badge'
+'use client';
+
+import ChangePasswordForm from '@/app/manage/setting/change-password-form';
+import UpdateProfileForm from '@/app/manage/setting/update-profile-form';
+import { Badge } from '@/components/ui/badge';
+import { Role } from '@/constants/type';
+import { useAppContext } from '@/components/app-provider';
 
 export default function Setting() {
+  const { role } = useAppContext();
+
   return (
     <main className='grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8'>
       <div className='mx-auto grid w-full flex-1 auto-rows-max gap-4'>
         <div className='flex items-center gap-4'>
-          <h1 className='flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0'>Cài đặt</h1>
+          <h1 className='flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0'>
+            Cài đặt
+          </h1>
           <Badge variant='outline' className='ml-auto sm:ml-0'>
-            Owner
+            {role}
           </Badge>
         </div>
         <div className='grid gap-4 md:grid-cols-2 md:gap-8'>
@@ -18,5 +26,5 @@ export default function Setting() {
         </div>
       </div>
     </main>
-  )
+  );
 }
