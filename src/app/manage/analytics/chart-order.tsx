df5c32a9c7c813d5f4be07c3a61ee57ad8b285e1
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-11-12 15:22:52 +0700

    fix line chart

--- src/app/manage/analytics/chart-order.tsx
+++ src/app/manage/analytics/chart-order.tsx
@@ -1,11 +1,21 @@
 'use client';
 
 import React, { useMemo } from 'react';
-import { Area, AreaChart, Bar, BarChart, CartesianGrid, XAxis } from 'recharts';
+import {
+  Area,
+  AreaChart,
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Line,
+  LineChart,
+  XAxis,
+} from 'recharts';
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
@@ -25,6 +35,7 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import { useGetRevenueQuery } from '@/queries/useOrder';
+import { TrendingUp } from 'lucide-react';
 
 // const chartData = [
 //   { date: '2024-06-29', paidOrders: 103, totalOrders: 160 },
@@ -32,9 +43,6 @@ import { useGetRevenueQuery } from '@/queries/useOrder';
 // ];
 
 const chartConfig1 = {
-  visitors: {
-    label: 'Visitors',
-  },
   paidOrders: {
     label: 'Tổng đơn đã thanh toán',
     color: 'hsl(var(--chart-1))',
@@ -76,77 +84,25 @@ export default function ChartOrder() {
     <>
       <div>
         <Card>
-          <CardHeader className='flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row'>
-            <div className='grid flex-1 gap-1 text-center sm:text-left'>
-              <CardTitle>Biểu đồ doanh thu 3 tháng</CardTitle>
-              <CardDescription>
-                Tổng doanh thu các đơn hàng đã thanh toán trong 3 tháng gần nhất
-              </CardDescription>
-            </div>
-            <Select value={timeRange} onValueChange={setTimeRange}>
-              <SelectTrigger
-                className='w-[160px] rounded-lg sm:ml-auto'
-                aria-label='Select a value'
-              >
-                <SelectValue placeholder='Last 3 months' />
-              </SelectTrigger>
-              <SelectContent className='rounded-xl'>
-                <SelectItem value='90d' className='rounded-lg'>
-                  3 tháng gần nhất
-                </SelectItem>
-                <SelectItem value='30d' className='rounded-lg'>
-                  30 ngày gần nhất
-                </SelectItem>
-                <SelectItem value='7d' className='rounded-lg'>
-                  7 ngày gần nhất
-                </SelectItem>
-              </SelectContent>
-            </Select>
+          <CardHeader>
+            <CardTitle>Biểu đồ đường </CardTitle>
+            <CardDescription>
+              Số lượng đơn hàng trong 3 tháng gần nhất
+            </CardDescription>
           </CardHeader>
-          <CardContent className='px-2 pt-4 sm:px-6 sm:pt-6'>
+          <CardContent>
             <ChartContainer
               config={chartConfig1}
               className='aspect-auto h-[250px] w-full'
             >
-              <AreaChart data={filteredData}>
-                <defs>
-                  <linearGradient
-                    id='fillPaidOrders'
-                    x1='0'
-                    y1='0'
-                    x2='0'
-                    y2='1'
-                  >
-                    <stop
-                      offset='5%'
-                      stopColor='var(--color-paidOrders)'
-                      stopOpacity={0.8}
-                    />
-                    <stop
-                      offset='95%'
-                      stopColor='var(--color-paidOrders)'
-                      stopOpacity={0.1}
-                    />
-                  </linearGradient>
-                  <linearGradient
-                    id='fillTotalOrders'
-                    x1='0'
-                    y1='0'
-                    x2='0'
-                    y2='1'
-                  >
-                    <stop
-                      offset='5%'
-                      stopColor='var(--color-totalOrders)'
-                      stopOpacity={0.8}
-                    />
-                    <stop
-                      offset='95%'
-                      stopColor='var(--color-totalOrders)'
-                      stopOpacity={0.1}
-                    />
-                  </linearGradient>
-                </defs>
+              <LineChart
+                accessibilityLayer
+                data={filteredData}
+                margin={{
+                  left: 12,
+                  right: 12,
+                }}
+              >
                 <CartesianGrid vertical={false} />
                 <XAxis
                   dataKey='date'
@@ -164,34 +120,23 @@ export default function ChartOrder() {
                 />
                 <ChartTooltip
                   cursor={false}
-                  content={
-                    <ChartTooltipContent
-                      labelFormatter={(value) => {
-                        return new Date(value).toLocaleDateString('en-US', {
-                          month: 'short',
-                          day: 'numeric',
-                        });
-                      }}
-                      indicator='dot'
-                    />
-                  }
+                  content={<ChartTooltipContent />}
                 />
-                <Area
+                <Line
                   dataKey='totalOrders'
-                  type='natural'
-                  fill='url(#fillTotalOrders)'
+                  type='monotone'
                   stroke='var(--color-totalOrders)'
-                  stackId='a'
+                  strokeWidth={2}
+                  dot={false}
                 />
-                <Area
+                <Line
                   dataKey='paidOrders'
-                  type='natural'
-                  fill='url(#fillPaidOrders)'
+                  type='monotone'
                   stroke='var(--color-paidOrders)'
-                  stackId='a'
+                  strokeWidth={2}
+                  dot={false}
                 />
-                <ChartLegend content={<ChartLegendContent />} />
-              </AreaChart>
+              </LineChart>
             </ChartContainer>
           </CardContent>
         </Card>
@@ -200,9 +145,9 @@ export default function ChartOrder() {
         <Card>
           <CardHeader className='flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row'>
             <div className='flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-6'>
-              <CardTitle>Bar Chart - Interactive</CardTitle>
+              <CardTitle>Biểu đồ cột</CardTitle>
               <CardDescription>
-                Showing total visitors for the last 3 months
+                Doanh thu trong 3 tháng gần nhất
               </CardDescription>
             </div>
           </CardHeader>
